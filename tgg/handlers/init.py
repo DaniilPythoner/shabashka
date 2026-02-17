b7773df26810c3a438417297ceb0925ea33d0307
DaniilPythoner <121360339+DaniilPythoner@users.noreply.github.com>
2026-02-18 00:05:52 +0300

    Add files via upload

---
+++ tgg/handlers/init.py
@@ -0,0 +1,7 @@
+# handlers/__init__.py
+from . import user
+from . import admin
+from . import http_bind
+from . import admin_game_control
+
+__all__ = ["user", "admin", "http_bind", "admin_game_control"]
