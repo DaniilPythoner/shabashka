40f32fa2454bd7ece7acc285be3f2aa524d8c0cd
DaniilPythoner <121360339+DaniilPythoner@users.noreply.github.com>
2026-02-17 23:14:36 +0300

    Delete tgg directory

--- tgg/handlers/init.py
+++
@@ -1,7 +0,0 @@
-# handlers/__init__.py
-from . import user
-from . import admin
-from . import donation_payments
-from . import admin_donation
-
-__all__ = ["user", "admin", "donation_payments", "admin_donation"]
