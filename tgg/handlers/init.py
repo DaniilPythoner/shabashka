70c2e84c28105680afb0c3f4000569101fa88096
DaniilPythoner <121360339+DaniilPythoner@users.noreply.github.com>
2026-02-19 16:38:13 +0300

    Delete tgg directory

--- tgg/handlers/init.py
+++
@@ -1,7 +0,0 @@
-# handlers/__init__.py
-from . import user
-from . import admin
-from . import http_bind
-from . import admin_game_control
-
-__all__ = ["user", "admin", "http_bind", "admin_game_control"]
